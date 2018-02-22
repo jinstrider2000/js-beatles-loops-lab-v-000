f53e02caa82e446435d959a0154ead71236ca354
Efrain Perez Jr <efrainperezjr@live.com>
2018-02-22 20:01:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
     array.push("${musicians[i]} plays ${instruments[i]}")
   }
+  return array
 }
 
 function johnLennonFacts(factsArray) {
-  return factsArray
+  var array = []
+  for (var i = 0; i < factsArray.length; i++) {
+    array.push(factsArray[i] + "!!!")
+  }
+  return array
+}
+
+function iLoveTheBeatles(num) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    num++
+  } while (num < 15);
 }
